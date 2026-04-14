eb264f988fe7a38cf172ad0654b07d1fcc72df60
David Y. Stephenson <david@davidystephenson.com>
2026-04-13 20:39:20 -0400

    Test generics

--- index.ts
+++ index.ts
@@ -1,19 +1,5 @@
-type Message = string | number
+type Message <T> = string | number | T
 
-const message1 = Math.random()
-console.log(message1)
+const message1: Message<boolean> = true
 
-const message2 = 100
-console.log(message2)
-
-function printMessage (message: Message) {
-  console.log('Here is the message:', message)
-  return 'hello'
-}
-const result = printMessage(message1)
-console.log('result.length', result.length)
-
-function saveMessage (message: Message) {
-  const string = String(message)
-  localStorage.setItem('message', string)
-}
+const message2: Message<[5]> = [5]
