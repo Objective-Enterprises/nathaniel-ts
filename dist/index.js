a1f9f48ac5683925a7532cd01c2856324b9de1b2
David Y. Stephenson <david@davidystephenson.com>
2026-04-15 20:18:17 -0400

    Generic function examples

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,21 @@
 "use strict";
-// type Message = 'hello' | 100 | boolean 
 Object.defineProperty(exports, "__esModule", { value: true });
-const message1 = Math.random();
-console.log(message1);
-const message2 = 100;
-console.log(message2);
-function printMessage(message) {
-    console.log('Here is the message:', message);
+const bakeryDelivery = {
+    from: 'flour factory',
+    to: 'bakery',
+    price: 1000,
+    cargo: 20 // how many bags
+};
+const schoolDelivery = {
+    from: "bus stop",
+    to: 'school',
+    price: 0,
+    cargo: ['Dorothy', 'Zelda', 'Tallulah']
+};
+function receiveDelivery(cargo) {
+    console.log('I received this cargo:', cargo);
+    return cargo;
 }
-printMessage(message1);
+const bakeryReceived = receiveDelivery(bakeryDelivery.cargo);
+const schoolRecevied = receiveDelivery(schoolDelivery.cargo);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,5 +1,36 @@
-type Message <T> = string | number | T
+interface Delivery <Cargo> {
+  from: string
+  to: string
+  price: number
+  cargo: Cargo
+}
+const bakeryDelivery: Delivery<number> = {
+  from: 'flour factory',
+  to: 'bakery',
+  price: 1000,
+  cargo: 20 // how many bags
+}
+const schoolDelivery: Delivery<string[]> = {
+  from: "bus stop",
+  to: 'school',
+  price: 0,
+  cargo: ['Dorothy', 'Zelda', 'Tallulah']
+}
 
-const message1: Message<boolean> = true
+function receiveDelivery <T> (cargo: T): T {
+  console.log('I received this cargo:', cargo)
+  return cargo
+}
 
-const message2: Message<[5]> = [5]
+const bakeryReceived = receiveDelivery(bakeryDelivery.cargo)
+const schoolReceived = receiveDelivery(schoolDelivery.cargo)
+function formatNumber (value: number) {
+  const formatted = value.toFixed(2) // 5 -> 5.00
+  console.log(formatted)
+}
+formatNumber(bakeryReceived)
+function formatString (value: string[]) {
+  const formatted = value.map(v => v.toUpperCase())
+  console.log(formatted)
+}
+formatString(schoolReceived)
